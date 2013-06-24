0e5b256e9d09180ff1c9837c590a680173f53c4a
akasharora <akasharora90@gmail.com>
2013-06-24 13:13:38 +0530

    exercise 2

--- exercise2/js/sandbox.js
+++ exercise2/js/sandbox.js
@@ -1,26 +1,27 @@
-var images = $('img');
+var images = $('img');                                            
 images.each(function() {
-    console.log("value of alt is " + $(this).attr('alt'));
+    console.log("value of alt is " + $(this).attr('alt'));          //displaying alt attribute of all selected images using $('img')
 });
 
-var search_input = $(':input[name = "q"]');
+
+var search_input = $(':input[name = "q"]');                         //selecting input where name = "q"(attribute of search input textbox)
 console.log(search_input);
-console.log(search_input.prevAll());
-search_input.parent().addClass('input_text');
+search_input.parent().addClass('input_text');                       //adding 'input_text' class to parent of 'search_input' i.e. the form here
 
-var list_item = $('#myList li.current').removeClass('current');
-list_item.addClass('input_class');
-console.log(list_item);
 
-var select = $('#specials select[name = "day"]');
+
+var list_item = $('#myList li.current').removeClass('current');     //removing class 'current' from list item having class 'current'
+list_item.next().addClass('current');                               //add class 'current' to next list item
+
+
+
+var select = $('#specials select[name = "day"]');                   //selecting select inside 'specials' id
 console.log(select);
-select.parent().next();
+console.log(select.parent().next().children());                     //selecting submit which here is child of next list item(after select)
 
-var submit = $('#specials input[type = "submit"]');
-console.log(select.parent().next());
 
-var list_item_1 = $('#slideshow li:first-child');
-list_item_1.addClass('current');
+
+var list_item_1 = $('#slideshow li:first-child');                   //selecting first li under id 'slideshow' id & add class current to it
+list_item_1.addClass('current');                                    
 console.log(list_item_1);
-var list_items = $('#slideshow li:nth-of-type(n + 2)').addClass('disabled');
-console.log($('#slideshow li:nth-of-type(n + 2)'));
+console.log(list_item_1.nextAll().addClass('disabled'))             //add class 'disabled' to next siblings of selected li
