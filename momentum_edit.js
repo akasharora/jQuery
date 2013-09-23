fd0d63db23c75cbfe0db9cce17538c8c41e3f43a
akasharora <akasharora90@gmail.com>
2013-09-23 18:53:36 +0530

    Update momentum_edit.js

--- momentum_edit.js
+++ momentum_edit.js
@@ -1,5 +1,5 @@
-<script type = "text/javascript>"
-   $("#arrow_image").hide();
+/*----------------------------------edited gby Akash-----------------------------*/
+$("#arrow_image").hide();
    $("#divisions").find("img").attr("data-select" , "0");
    $("#divisions").find("img").attr("id" , function(i){
    		return ("img"+i);
@@ -7,7 +7,15 @@
    $(".config").find(".item").attr("id",function(j){
    		return ("item"+j);
    });
+   /*-------creating a link to be appended when thumbnail is clicked upon----------*/
+    var aTag = document.createElement('a');
+	aTag.setAttribute('href',"javascript:void(0)");
+	aTag.setAttribute('data-filter','');
+	aTag.innerHTML = "click here to view showcase";
+	aTag.setAttribute("id","click_here");
+
    	$("#arrow_image img").unbind("click");
+
     $("#divisions .thumbnail").on('click',function(){
 		var content = $(this).parent().find("p").html();
 		var divImgs = $("#divisions .thumbnail");
@@ -16,31 +24,35 @@
 		var apendAfter=(Math.floor(place/4) + 1)*4 - 1;
 		if (apendAfter > elementCnt){ apendAfter = elementCnt};
 
-
 		if($(this).children('img').attr("data-select")==0){
-			var wait = 200;
+			var wait = 150;
+
 			if($("#show_hide").css("height") == "0px") {
 				wait = 0;
-			} 
+			}
 			$("#show_hide").css("height","0px");
 			setTimeout(function(){
-				//$("#divisions .item").eq(place).append($("#arrow"));
 				$("#divisions .item").eq(place).append($("#arrow_image"));
 				$("#divisions .item").eq(apendAfter).after($("#show_hide"));
+
 				setTimeout(function(){
 					$("#content1").html(content);
-					$("#content1").append('/<br/>');
+					$("body").scrollTo("#divisions",1000);
+					$("#content1").append('<br/>');
 					$("#arrow_image").show();
-					var element = document.createElement("input");
-					element.setAttribute("id","click_here_to_view");
-					element.setAttribute("type","button");
-					element.setAttribute("value","click here to view showcase");
-					$("#content1").append(element);
-					$("#show_hide").css("height","165px");
-					$("body").on("click","#click_here_to_view",function()
-						{
-							$("body").scrollTo("#portfolio",1500);
-						});
+					$("#content1").append(aTag);
+					$("#show_hide").css("height","175px");
+					var data_string = $(".thumbnail").eq(place).find('img').attr('alt');
+					data_string = data_string.toLowerCase();
+					data_string = '.' + data_string;
+					data_string = data_string.replace(/\s+/g, '-');
+					$("body").on('click',"#click_here",function(){
+						$("body").scrollTo("#portfolio",1000);
+						var data_filter = $(".item div:first-child").eq(place).attr('data-name');
+						$("a[data-filter='"+data_string+"']").click();
+						data_string = null;
+ 
+					});
 				},10);
 			},wait);
 			$("#divisions").find("img").attr("data-select" , "0");
@@ -48,16 +60,13 @@
 		}
 		else{
 			$(this).children('img').attr("data-select","0");
-			$("#content1").html('');
+			//$("#content1").html('');
 			$("#show_hide").css("height","0px");
 			$("#arrow_image").hide();
 		}
 	});
 	$("#show_hide").find("img").on('click',function(){
 		$("#show_hide").css("height","0px");
-		$('body').scrollTo("#wrapper");
 		$("#divisions").find("img").attr("data-select" , "0");
 		$("#arrow_image").hide();
 	});
-</script>
-/*------------------------------------edit complete-------------------------------*/
